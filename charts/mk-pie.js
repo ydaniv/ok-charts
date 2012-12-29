d5b4e85f936c0c7072c96a3b06a3a169ec839902
Yehonatan Daniv <maggotfish@gmail.com>
2012-12-29 19:08:32 +0200

    Implemented inner/outer radi calculations and canvas positioning; Changed colors option to classes

--- charts/mk-pie.js
+++ charts/mk-pie.js
@@ -17,10 +17,10 @@
             padding     : 10,
             line_width  : 5,
             radius      : 300 / 2,
-            colors      : {
-                yes     : 'green',
-                no      : 'red',
-                didnt   : 'transparent'
+            classes     : {
+                yes     : 'good',
+                no      : 'bad',
+                didnt   : 'no-vote'
             },
             sort        : null,
             value       : function (d) { return d.votes; }
@@ -30,24 +30,35 @@
             for ( op in options ) {
                 this.options[op] = options[op];
             }
+            if ( ! ('radius' in options) ) {
+                this.options.radius = Math.min(this.options.width, this.options.height) / 2;
+            }
         }
+
+        var line_width = this.options.line_width,
+            line_width_factor = line_width / 2;
+        this.options.width = this.options.width + line_width;
+        this.options.height = this.options.height + line_width;
 
         // arc object definition
         this.arc = d3.svg.arc()
-                        .outerRadius(this.options.radius - this.options.padding)
-                        .innerRadius(this.options.radius - this.options.line_width);
+            .outerRadius(this.options.radius - this.options.padding + line_width_factor)
+            .innerRadius(this.options.radius - this.options.padding - line_width_factor);
 
         // pie layout
         this.pie = d3.layout.pie()
-                        .sort(this.options.sort)
-                        .value(this.options.value);
+            .sort(this.options.sort)
+            .value(this.options.value);
 
         // canvas
         this.canvas = d3.select(element).append('svg')
-                .attr('width', this.options.width)
-                .attr('height', this.options.height)
-                .append('g')
-                    .attr('transform', 'translate(' + this.options.width / 2 + ',' + this.options.height / 2 + ')');
+            .attr('width', this.options.width)
+            .attr('height', this.options.height)
+            .style('position', 'relative')
+            .style('top', -line_width_factor + 'px')
+            .style('left', line_width_factor + 'px')
+            .append('g')
+            .attr('transform', 'translate(' + this.options.width / 2 + ',' + this.options.height / 2 + ')');
     }
 
     MKPieChart.prototype = {
@@ -55,15 +66,17 @@
         render      : function (data) {
             // arc containers
             var g = this.canvas.selectAll('.arc')
-                                .data(this.pie(data))
-                                .enter().append('g')
-                                .attr('class', 'arc'),
-                colors = this.options.colors;
+                    .data(this.pie(data))
+                    .enter().append('g')
+                    .attr('class', 'arc'),
+                classes = this.options.classes;
 
             // arcs
             g.append('path')
                 .attr('d', this.arc)
-                .style('fill', function(d) { return colors[d.data.type]; });
+                .classed('good', function(d) { return classes[d.data.type] == 'good'; })
+                .classed('bad', function(d) { return classes[d.data.type] == 'bad'; })
+                .classed('no-vote', function(d) { return classes[d.data.type] == 'no-vote'; });
 
             return this;
         }
