6e00cee8b078f6f0cc206ab74706b717a444f486
Yehonatan Daniv <maggotfish@gmail.com>
2013-05-08 12:00:04 +0300

    Updated test to reflect what's on OK site

--- examples/pie-test.js
+++ examples/pie-test.js
@@ -6,19 +6,23 @@ requirejs.config({
 requirejs([
     '../charts/mk-pie'
 ], function (MKPieChart) {
-    var testdata = [{
-        label   : 'Yes',
-        type    : 'yes',
-        votes   : 5
-    }, {
-        label   : "Didn't Vote",
-        type    : 'didnt',
-        votes   : 10
-    }, {
-        label   : 'No',
-        type    : 'no',
-        votes   : 8
-    }];
 
-    var chart = new MKPieChart('#chart').render(testdata);
+    var chart = document.getElementById('chart'),
+        data_attrs = chart.dataset,
+        pie_data = [],
+        pie_order = ['for', 'noVote', 'abstain', 'against'],
+        k;
+    
+    for (k in data_attrs) {
+        pie_data.push({type: k, votes: data_attrs[k]});
+    }
+    pie_data.sort(function (a, b) {
+        return pie_order.indexOf(a.type) - pie_order.indexOf(b.type);
+    });
+    new MKPieChart(chart, {
+        width       : 90,
+        height      : 89,
+        padding     : 0,
+        line_width  : 6
+    }).render(pie_data);
 });
